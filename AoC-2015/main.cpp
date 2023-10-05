8df6a94751d9a3263a4902e6dc91b94747d8ab3d
joao_arnold.py <joaopaulo167@gmail.com>
2023-10-05 00:16:26 -0300

    Initial Commit

---
+++ AoC-2015/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AoC-2015
+//
+//  Created by João Paulo Arnold Barros on 05/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
